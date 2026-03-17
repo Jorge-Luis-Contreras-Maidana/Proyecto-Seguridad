6698f889ea22d66b89842278b896aa46c288399d
Lans776 <147963942+Lans776@users.noreply.github.com>
2026-03-17 16:17:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.secret_key = "universidad_llave_maestra"
 
 # --- CONFIGURACIÓN DE BASE DE DATOS ---
+# --Cambio Ejemplo
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
